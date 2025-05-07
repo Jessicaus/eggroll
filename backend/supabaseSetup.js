e3947d9dd23413556a899345e92118890083f29b
Reva-Mahajan <146595211+Reva-Mahajan@users.noreply.github.com>
2025-05-06 22:09:03 -0700

    Update supabaseSetup.js to .env

--- backend/supabaseSetup.js
+++ backend/supabaseSetup.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-dotenv.config({ path: path.join(__dirname, 'supabase.env') })
+dotenv.config({ path: path.join(__dirname, '.env') })
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
 
