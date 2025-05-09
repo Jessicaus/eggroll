bfd64a391b26f8fb578d73918e1090737967c8ac
JoyceLiao2022 <Joyceliao2022@gmail.com>
2025-05-08 21:54:47 -0700

    Change Login to update the fetch command

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -12,7 +12,7 @@ export default function Login() {
     e.preventDefault();
     setErrorMsg('');
 
-    const response = await fetch('http://localhost:3000/auth/login', {
+    const response = await fetch('http://localhost:3000/api/auth/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username, password }),
