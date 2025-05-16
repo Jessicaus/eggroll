6c617fd5c973279384ea5aa977792edb3c950206
Rachel Chan <rachelchan@Rachels-Laptop.local>
2025-05-15 22:03:58 -0700

    super simple check-in page, code validation, and update of attendance table (still WIP)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,14 @@
 //import viteLogo from '/vite.svg'
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+
+// add pages
 import Login from './Pages/Login';
 import Register from './Pages/Register';
 import Home from './Pages/Home';
 import CreateEvent from './Pages/CreateEvent';
+import CheckIn from './Pages/CheckIn';
+
 import './App.css';
 
 function App() {
@@ -16,6 +20,7 @@ function App() {
       <Route path="/signup" element={<Register />} />
       <Route path="/home" element={<Home />} />
       <Route path ="/create" element = {<CreateEvent />} />
+      <Route path="/checkin" element={<CheckIn />} />
     </Routes>
   )
 }
