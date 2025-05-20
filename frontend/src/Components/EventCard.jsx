7f2ff291cc70762029cc18b1bab1c93f87d3f446
Rachel Chan <rachelchan@wifi-169-232-242-147.host.ucla.edu>
2025-05-20 14:51:22 -0700

    Update EventCard before merge

--- frontend/src/Components/EventCard.jsx
+++ frontend/src/Components/EventCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import './EventCard.css';
 
 const EventCard = () => (
@@ -6,12 +7,13 @@ const EventCard = () => (
     <div className="card">
       <div className="card-form clickable-area">
         <div className="title">Event 1</div>
-        <p>"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti
-</p>
+        <p>"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti</p>
       </div>
       <div className="card-footer">
+        <Link to="/checkin">
           <button className="checkin-btn">Check In</button>
-        </div>
+        </Link>
+      </div>
     </div>
   </main>
 );
