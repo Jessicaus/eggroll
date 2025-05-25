8a62a73169125430ed7d8a9edbcba8c0aba070ce
Reva Mahajan <reva.mahajan@icloud.com>
2025-05-25 12:53:19 -0700

    changing authroutes to not log request

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,7 +6,6 @@ import createUser from './../auth/createUser.js'
 import loginUser from './../auth/loginUser.js'
 
 router.post('/login', async (req, res) => {
-  console.log('Login Request Body:', req.body);
   try {
     const result = await loginUser(req.body);
     res.status(200).json(result);
