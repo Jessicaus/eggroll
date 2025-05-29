eb4b573d5bc8ac0725d7a2f35e994ffea3e42fc3
Reva Mahajan <reva.mahajan@icloud.com>
2025-05-29 14:38:55 -0700

    added backend routes for changing the live status of events

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router(); //creates a router
 
 import createEvent from './../events/createEvent.js';
 import searchEvent from './../events/searchEvent.js';
+import liveEvent from './../events/liveEvent.js';
 
 router.post('/create', async (req, res) => {
   console.log('Event Create Request Body:', req.body);
@@ -31,6 +32,17 @@ router.get('/search', async (req, res) => {
   } catch (err) {
     console.error('Search error: ', err);
     res.status(500).json({ error: err.message });
+  }
+});
+
+
+router.post('/live', async (req, res) => {
+  console.log('changing live status')
+  try {
+    const result = await liveEvent(req.body.id, req.body.status);//change live status based on what is in the body of the request
+    res.status(200).json(result);//if successful, send a 200 response
+  } catch (err) {
+    res.status(400).json({ error: err.message });//if error, send a 400 response
   }
 });
 
--- frontend/src/Components/EventCard.jsx
+++ frontend/src/Components/EventCard.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import {supabase} from '../../supabaseClient.js'; // or adjust path as needed
 import './EventCard.css';
 
 const EventCard = ({event, viewType}) => {
@@ -11,14 +10,24 @@ const EventCard = ({event, viewType}) => {
     console.log('Toggle clicked, new value:', newValue);
     setIsLive(newValue);  // update local toggle state
 
-    const { error } = await supabase
-      .from('events')
-      .update({ is_live: newValue })
-      .eq('id', event.id);
+    
+    const response = await fetch('http://localhost:3000/api/events/live', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        id: event.id,
+        status: newValue
+      }),
+    });
 
-    if (error) {
-      console.error('Error updating is_live:', error);
+    if (response.ok) {
+      console.log("from frontend: livestatus successfully changed")
+    } else {
+      console.log("from frontend: livestatus change failed")
+      
     }
+  
+  
   };
 
   const formattedStart = new Date(event.event_start_time).toLocaleString('en-US', {
